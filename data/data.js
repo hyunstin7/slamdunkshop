3959a2d3eafe5970a713771d19e8949ad0603c21
최현선 <pigethang7@naver.com>
2023-09-06 12:40:43 +0900

    슬램덩크홈페이지

---
+++ data/data.js
@@ -0,0 +1,54 @@
+let Statue = [
+    {id : 0 , title : "1" , price: 10000, option:[], img:'./img/Statue1.jpg'},
+    {id : 1 , title : "2" , price: 10000, option:[], img:'./img/Statue2.jpg'},
+    {id : 2 , title : "3" , price: 10000, option:[], img:'./img/Statue3.jpg'},
+    {id : 3 , title : "3" , price: 10000, option:[], img:'./img/Statue4.jpg'},
+    {id : 4 , title : "3" , price: 10000, option:[], img:'./img/Statue5.jpg'},
+
+]
+
+let Figure = [
+    {id : 0 , title : "4" , price: 10000, option:[], img:'./img/poster1.png'},
+    {id : 0 , title : "5" , price: 10000, option:[], img:'./img/poster1.png'},
+    {id : 0 , title : "6" , price: 10000, option:[], img:'./img/poster1.png'},
+
+]
+
+let Tshirt = [
+    {id : 0 , title : "7" , price: 10000, option:[], img:'./img/poster1.png'},
+    {id : 0 , title : "8" , price: 10000, option:[], img:'./img/poster1.png'},
+    {id : 0 , title : "9" , price: 10000, option:[], img:'./img/poster1.png'},
+
+]
+
+let Uniform = [
+    {id : 0 , title : "10" , price: 10000, option:[], img:'./img/poster1.png'},
+    {id : 0 , title : "11" , price: 10000, option:[], img:'./img/poster1.png'},
+    {id : 0 , title : "12" , price: 10000, option:[], img:'./img/poster1.png'},
+
+]
+
+let Shoes = [
+    {id : 0 , title : "13" , price: 10000, option:[], img:'./img/poster1.png'},
+    {id : 0 , title : "14" , price: 10000, option:[], img:'./img/poster1.png'},
+    {id : 0 , title : "15" , price: 10000, option:[], img:'./img/poster1.png'},
+
+]
+
+let Poster = [
+    {id : 0 , title : "채치수 포스터" , price: 10000, option:[], img:'./img/poster1.png'},
+    {id : 1 , title : "강백호 포스터" , price: 10000, option:[], img:'./img/poster2.png'},
+    {id : 2 , title : "정대만 포스터" , price: 10000, option:[], img:'./img/poster3.png'},
+    {id : 3 , title : "송태섭 포스터" , price: 10000, option:[], img:'./img/poster4.png'},
+    {id : 4 , title : "서태웅 포스터" , price: 10000, option:[], img:'./img/poster5.png'},
+
+]
+
+let Accesory = [
+    {id : 0 , title : "" , price: 10000, option:[], img:'./img/poster1.png'},
+    {id : 0 , title : "" , price: 10000, option:[], img:'./img/poster1.png'},
+    {id : 0 , title : "" , price: 10000, option:[], img:'./img/poster1.png'},
+
+]
+
+export {Statue, Figure, Tshirt, Uniform, Shoes, Poster, Accesory};
